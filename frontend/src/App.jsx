10f4c4f717e5ae6154ab5eef37fb83a299814e81
HarshJaiswal02 <harshjaiswal0204@gmail.com>
2024-09-22 12:27:06 +0530

    Frontend initialized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Frontend Of Chat - App</h1>
+      <h1 className="text-3xl font-bold underline">Frontend Of Chat - App</h1>
     </>
   );
 }
