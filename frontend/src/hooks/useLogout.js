98c68f7a9e394ffdca3bb7f4a53bc92696d7427f
HarshJaiswal02 <harshjaiswal0204@gmail.com>
2024-09-29 16:45:12 +0530

    Logout Feature Implemented

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../contextApis/AuthContext";
+
+export const useLogout = () => {
+  const { setAuthUser } = useAuthContext();
+  const [loading, setLoading] = useState(false);
+
+  const logout = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = res.json();
+      console.log(data);
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      localStorage.clear("chat-user");
+      setAuthUser(null);
+      
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { loading, logout };
+};
