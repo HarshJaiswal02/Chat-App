1bccd46f823fa1220cf150616764331328c3408f
HarshJaiswal02 <harshjaiswal0204@gmail.com>
2024-10-15 09:57:19 +0530

    Depployment task

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import cookieParser from "cookie-parser";
 import authRoutes from "../backend/routes/auth.routes.js";
 import messageRoutes from "../backend/routes/message.routes.js";
 import userRoutes from "../backend/routes/users.routes.js";
-
+import path from "path";
+import { fileURLToPath } from "url";
 import DbConnect from "./config/db.js";
 import { app, server } from "./socket/socket.js";
 
@@ -16,12 +17,17 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json()); //Parse the incoming request with JSON payload (body-parser)
 
+const __dirname = path.resolve();
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 const PORT = process.env.PORT || 8000;
 
 DbConnect()
